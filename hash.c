5bd346fff290f101db445f266a41d9f9eb83e032
xinoir <xinoir@theta.black>
2017-08-07 16:33:37 -0500

    initial commit for a hash

---
+++ hash.c
@@ -0,0 +1,39 @@
+/*
+ * hash routines
+ * Copyright (C) 2013-2017 xinoir <xinoir@theta.black> 
+ * This file is part of beta.
+ */
+
+#include <math.h>
+#include "beta/hash.h"
+
+/* 
+ * (sqrt(5)-1)/2
+ *
+ * [0] Donald E. Knuth. Sorting and Searching, volume 3 of The Art of Computer
+ * Programming. Addison-Wesley, 1998. 2nd ed.
+ *
+ * [1] Cormen, Thomas H.; Leiserson, Charles E.; Rivest, Ronald L.; Stein,
+ * Clifford. Introduction to Algorithms. The MIT Press, 2009. 3rd ed.
+ */
+
+#define A 0.6180339887498949
+
+unsigned hash(int i, int n)
+{
+    unsigned h;
+    unsigned long w, p, s, r, ra, rb;
+
+    w = sizeof(int) * 8;
+    p = (unsigned long)(ceil(log2(n)));
+    s = (unsigned long)(A * pow(2, w));
+    r = (unsigned)i * s;
+    ra = r >> w;
+    rb = r ^ (ra << w);
+    h = (unsigned)(rb >> (w-p));
+
+    return h;
+}
+
+#undef A
+
---
+++ hash.h
@@ -0,0 +1,13 @@
+/*
+ * hash routines
+ * Copyright (C) 2013-2017 xinoir <xinoir@theta.black> 
+ * This file is part of beta.
+ */
+
+#ifndef HASH_H
+#define HASH_H
+
+unsigned hash(int i, int n);
+
+#endif
+
