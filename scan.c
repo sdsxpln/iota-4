902c3ba1bb9202d85c63eea2ac04015a7cc0dc29
xinoir <xinoir@theta.black>
2017-08-18 09:44:09 -0500

    work in progress on scan being used as a test

--- scan.c
+++ scan.c
@@ -6,52 +6,24 @@
 #include <errno.h>
 #include "beta/error.h"
 #include "beta/notused.h"
-#include "beta/tree.h"
+#include "beta/stream.h"
 #include "beta/list.h"
-
-#define LINE_SIZE 4096
-#define WORD_SIZE 128
-
-static int get_line(FILE *input, char *line, int size)
-{
-    if (!fgets(line, size, input))
-        return error("failed to read a line from a stream");
-
-    return 0;
-}
-
-static int get_word(FILE *input, char *word)
-{
-    char c;
-
-    while ((c = fgetc(input)) != EOF && !isalpha(c))
-        ;
-    if (c == EOF)
-        return EOF;
-    for (*word = c; (c = fgetc(input)) && c != EOF; ) {
-        if (!isalpha(c)) {
-            ungetc(c, input);
-            break;
-        }
-        *++word= c;
-    }
-    *++word = 0;
-
-    return 0;
-}
-
-static int compare(const void *object1, const void *object2)
-{
-    return strcmp((const char *)object1, (const char *)object2);
-}
 
 int main(int argc, char **argv)
 {
     notused(argc);
     notused(argv);
-    int status = 0;
-    FILE *input = stdin;
-    FILE *output = stdout;
+    FILE *in = stdin;
+    FILE *out = stdout;
+    char w[256] = {0};
+    int infd;
+    int iseof = 0;
+    
+    infd = fileno(in);
+    while (!stream_read_word(infd, w, strlen(w), &iseof) && !iseof)
+        fprintf(out, "%s\n", w);
+
+/*
     char word[WORD_SIZE] = { 0 };
     char line[LINE_SIZE] = { 0 };
     struct tree *tree;
@@ -59,14 +31,13 @@ int main(int argc, char **argv)
     struct list_node *list_node;
     size_t line_length;
 
-/*
     tree_init(&tree, &compare);
     while ((status = get_word(input, word)) == 0 && status != EOF) {
         tree_add(tree, word, strlen(word) + 1);
     }
     tree_print(output, tree);
     tree_free(&tree);
-*/
+
     list_create(&list);
     while ((status = get_line(input, line, LINE_SIZE)) == 0 && status != EOF) {
         list_insert_node(list, line, strlen(line) + 1, &list_node);
@@ -74,7 +45,7 @@ int main(int argc, char **argv)
     for (list_node = list_head(list); list_node; list_node = list_next_node(list_node))
         fprintf(output, "%s", (char *)list_node_data(list_node, &line_length));
     list_destroy(&list);
-
+*/
     return 0;
 }
 
