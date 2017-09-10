9757a03789ad83a87cf5e20c084d194005dc9706
Roman Fakhrazeyev <xinoir@theta.black>
2017-09-10 15:36:11 +0000

    work in progress

--- map.c
+++ map.c
@@ -14,19 +14,19 @@ This file is part of beta.
 
 struct map {
     int size;
-    struct list **buckets;
     unsigned (* hash)(const void *, int);
     int (* compare)(const void*, const void *);
+    struct list **buckets;
 };
 
 static int map_num_of_objects(const struct map *map)
 {
     int n = 0;
-    struct list **b = map->buckets;
+    struct list **bucket = map->buckets;
 
-    for (int i = 0; i < map->size; i++, b++)
-        if (*b)
-            n += list_length(*b);
+    for (int i = 0; i < map->size; i++, bucket++)
+        if (*bucket)
+            n += list_length(*bucket);
 
     return n;
 }
@@ -40,10 +40,13 @@ void **map_to_array(const struct map *map, size_t *length)
 
     *length = map_num_of_objects(map);
     if (!(objects = malloc(sizeof(void *) * *length))) return NULL;
-    for (int i = 0; i < map->size; i++, bucket++)
-        if (*bucket)
-            for (node = list_head(*bucket); node; node = list_next(node))
+    for (int i = 0; i < map->size; i++, bucket++) {
+        if (*bucket) {
+            for (node = list_head(*bucket); node; node = list_next(node)) {
                 *objects++ = list_node_object(node);
+            }
+        }
+    }
 
     return objects - *length;
 }
@@ -53,10 +56,12 @@ int map_contains(const struct map *map, const void *key)
     struct list *list;
     struct list_node *node;
 
-    if ((list = *(map->buckets + map->hash(key, map->size))))
-        for (node = list_head(list); node; node = list_next(node))
+    if ((list = *(map->buckets + map->hash(key, map->size)))) {
+        for (node = list_head(list); node; node = list_next(node)) {
             if (map->compare(key, list_node_key(node)) == 0)
                 return 1;
+        }
+    }
 
     return 0;
 }
@@ -66,21 +71,24 @@ void *map_lookup(const struct map *map, const void *key)
     struct list *list;
     struct list_node *node;
 
-    if ((list = *(map->buckets + map->hash(key, map->size))))
-        for (node = list_head(list); node; node = list_next(node))
+    if ((list = *(map->buckets + map->hash(key, map->size)))) {
+        for (node = list_head(list); node; node = list_next(node)) {
             if (map->compare(key, list_node_key(node)) == 0)
                 return list_node_object(node);
+        }
+    }
 
     return NULL;
 }
 
 int map_install(struct map *map, const void *key, const void *object)
 {
-    struct list *bucket;
+    struct list **slot;
 
-    if (!(bucket = *(map->buckets + map->hash(key, map->size))))
-        bucket = list_create();
-    list_prepend(bucket, key, object);
+    slot = map->buckets + map->hash(key, map->size);
+    if (!*slot)
+        *slot = list_create();
+    list_prepend(*slot, key, object);
 
     return 0;
 }
